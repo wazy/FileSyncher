715530943fe509b4deb675f10d8297da8a7fcdb7
Testing this <biobud74@yahoo.com>
2012-12-18 02:47:02 -0500

    some changes'

--- client.c
+++ client.c
@@ -15,6 +15,35 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+
+
+ /*   
+    struct stat sb;
+
+    stat(argv[1], &sb);
+    char s[256];
+    strcpy(s, ctime(&sb.st_mtime));
+    char *token = strtok(s, " ");
+    int count = 0;
+    while (token && count < 3) {
+        token = strtok(NULL, " ");
+        count++;
+    }
+    printf("%s\n", token);
+    strcpy(s, token);
+    token = strtok(s, ":");
+    count = 0;
+    printf("%s\n", token);
+    int x = (int *)token - (int *)token;
+    printf("%d\n", x);
+    while (token && count < 1) {
+        token = strtok(NULL, ":");
+        count++;
+    }
+    printf("%s\n", token);
+
+*/
+
 int main(int argc, char *argv[])
 {
     if (argc < 2)
@@ -53,29 +82,7 @@ int main(int argc, char *argv[])
     
     // write(sockfd, argv[1], sizeof(argv[1])); // to send file name
     // open the user's file
-    struct stat sb;
 
-    stat(argv[1], &sb);
-    char s[256];
-    strcpy(s, ctime(&sb.st_mtime));
-    char *token = strtok(s, " ");
-    int count = 0;
-    while (token && count < 3) {
-        token = strtok(NULL, " ");
-        count++;
-    }
-    printf("%s\n", token);
-    strcpy(s, token);
-    token = strtok(s, ":");
-    count = 0;
-    printf("%s\n", token);
-    int x = (int *)token - (int *)token;
-    printf("%d", x);
-    while (token && count < 1) {
-        token = strtok(NULL, ":");
-        count++;
-    }
-    printf("%s\n", token);
     int fd;
     fd = open(argv[1], O_RDONLY);
    
@@ -101,6 +108,7 @@ int main(int argc, char *argv[])
             p += bytes_written;
         }
     }
+    printf("\nTranfer succeeded!\n\n");
     close(sockfd);
     exit(0);
 }
