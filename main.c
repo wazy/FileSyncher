ad62175c921495eac414a9476e0563515519ebb9
Testing this <biobud74@yahoo.com>
2012-12-19 02:33:12 -0500

    Big changes: Fixed compile error with header, implemented get… ting the file name of everything in the dynamic /home/user/FileSyncher directory, added two missing files and addressed some missing declarations, misc. cleanups, etc. {repush}

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <dirent.h>
+#include <stdio.h>
+#include "other.h"
+
+int main ()
+{
+	printf("%d\n", getfavoritenumber());
+	return 0;
+}
---
+++ splitter.c
@@ -0,0 +1,15 @@
+/* 
+ *  boolean splitter declaration 
+ *  for splitting timestamps
+ *  Version 0.01
+ *  ~12/19/12~
+ *
+*/
+
+#include "client_func.h"
+
+char *splitter(char *stat)
+{
+    printf("split\n");
+    return 0;
+}
